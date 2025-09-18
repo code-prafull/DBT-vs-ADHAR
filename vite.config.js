990ab1bd57599de6d30e257d5714ff3ea8381fb7
code-prafull <prafull.singh.cs@gmail.com>
2025-09-18 23:41:32 +0530

    Add deployment configuration for GitHub Pages and update Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
+  base: process.env.NODE_ENV === 'production' ? '/DBT-vs-ADHAR/' : '/',
   plugins: [
     tailwindcss(),
     react()],
